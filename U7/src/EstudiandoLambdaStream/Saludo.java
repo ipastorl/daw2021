9099cb4d7b63be1c8b22573369889649e1b53535
ipastorl <alum.ipastorl.2021@iesalixar.org>
2022-04-16 20:50:33 +0200

    isa ejercicios en casa

---
+++ U7/src/EstudiandoLambdaStream/Saludo.java
@@ -0,0 +1,14 @@
+package EstudiandoLambdaStream;
+
+public class Saludo {
+    private String nombre;
+
+    public Saludo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Hola, " + nombre ;
+    }
+}
